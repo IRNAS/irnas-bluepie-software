49d748d7ee46a68e8c8b7235b617f2ee63b80656
MarkoSagadin <marko.sagadin42@gmail.com>
2022-02-15 13:17:54 +0100

    add configurable logging levels

--- bluepie/logs.py
+++ bluepie/logs.py
@@ -3,7 +3,7 @@ from rich.logging import RichHandler
 
 
 class Logs(Logger):
-    def __init__(self, enable: bool = False):
+    def __init__(self, level: str = "INFO", enable: bool = False):
 
         """
         Initialises standard logging module with RichHandler for richer and
@@ -11,9 +11,17 @@ class Logs(Logger):
 
         Args:
             enabled (bool): whether the logging module will be enabled.
+            level (bool): logging level, possible options are:
+                - "CRITICAL"
+                - "ERROR"
+                - "WARNING"
+                - "INFO"
+                - "DEBUG"
+                more about logging levels in on this link:
+                https://docs.python.org/3/howto/logging.html#logging-levels
         """
         super().__init__("rich")
-        super().setLevel("INFO")
+        super().setLevel(level)
         super().addHandler(RichHandler(rich_tracebacks=True))
 
         # This controls whether the logs will be enabled or not
